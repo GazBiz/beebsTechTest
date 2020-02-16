f7f8a88dc7412db41110cf0f2334d5f8636d4c1c
Gaz Biz <gareth@resdiary.com>
2020-02-16 15:55:29 +0000

    create APP class and move error logging

--- app/src/main/java/com/example/garethbizleybeebstest/MainActivity.kt
+++ app/src/main/java/com/example/garethbizleybeebstest/MainActivity.kt
@@ -2,23 +2,11 @@ package com.example.garethbizleybeebstest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.garethbizleybeebstest.Constants.Companion.ERROR
-import com.example.garethbizleybeebstest.logging.LoggingServiceImpl
 
 class MainActivity : AppCompatActivity() {
-
-    private val loggingService = LoggingServiceImpl.instance
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupErrorLogger()
-    }
-
-    private fun setupErrorLogger(){
-        Thread.setDefaultUncaughtExceptionHandler { _, paramThrowable ->
-            loggingService.logEvent(ERROR, paramThrowable.localizedMessage)
-                .subscribe()
-        }
     }
 }
